702ece79775c655a3f8209003763f1a5c8a278ca
Marco Barreto <marcosouzabarreto@gmail.com>
2023-12-29 00:48:05 -0300

    Updated Tmux-Sessionizer

--- setup.sh
+++ setup.sh
@@ -22,5 +22,15 @@ link_file "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
 link_file "$DOTFILES_DIR/i3/config" "$HOME/.config/i3/config"
 link_file "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
 
+
 echo "Dotfiles setup complete."
 
+
+echo "Setting up your scripts"
+
+if [ -f "$DOTFILES_DIR/tmux-sessionizer" ]; then
+    mkdir -p /usr/local/bin 
+    link_file "$DOTFILES_DIR/tmux-sessionizer" "/usr/local/bin"
+fi
+
+echo "Scripts setup complete."

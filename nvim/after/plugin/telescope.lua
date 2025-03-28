d3f65e3ec16f51e9db10f8cd401c43a83ac1eb24
Marco Barreto <marcosouzabarreto@gmail.com>
2025-03-28 01:35:49 -0300

    feat: Update telescope to start on normal mode

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,12 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+
+telescope.setup {
+  defaults = {
+    initial_mode = "normal",
+  },
+}
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()

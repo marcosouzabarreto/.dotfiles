0e73dd48ae79a218d58ac5c83d80053d98afa194
Marco Barreto <marcosouzabarreto@gmail.com>
2025-04-01 14:11:00 -0300

    refactor: Remove mason -- breaking on nixos

--- nvim/lua/marcobarreto/plugins/lsp.lua
+++ nvim/lua/marcobarreto/plugins/lsp.lua
@@ -1,8 +1,6 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
@@ -12,7 +10,6 @@ return {
         "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
     },
-
     config = function()
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
@@ -23,100 +20,33 @@ return {
             cmp_lsp.default_capabilities())
 
         require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "gopls",
-                "pyright",
-            },
-            handlers = {
-                function(server_name) -- default handler
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-                ["pyright"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.pyright.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            python = {
-                                analysis = {
-                                    autoSearchPaths = true,
-                                    useLibraryCodeForTypes = true,
-                                    diagnosticMode = "workspace",
-                                },
-                            },
-                        },
-                        -- Override the default formatting handler
-                        on_attach = function(client, bufnr)
-                            -- Disable pyright's native formatting (if any)
-                            client.server_capabilities.documentFormattingProvider = false
 
-                            -- Define a custom format function
-                            local function format_with_black()
-                                local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-                                local formatted = vim.fn.systemlist("black --quiet -", lines)
-                                if vim.v.shell_error == 0 then
-                                    vim.api.nvim_buf_set_lines(0, 0, -1, false, formatted)
-                                else
-                                    vim.notify("Black formatting failed", vim.log.levels.ERROR)
-                                end
-                            end
+        local lspconfig = require("lspconfig")
 
-                            -- Map vim.lsp.buf.format() to our custom function for Python files
-                            vim.api.nvim_buf_create_user_command(bufnr, "LspFormat", function()
-                                format_with_black()
-                            end, { desc = "Format buffer with black" })
-
-                            -- Optional: Keybinding for convenience
-                            vim.keymap.set("n", "<leader>f", format_with_black, {
-                                buffer = bufnr,
-                                desc = "Format with black",
-                            })
-                        end,
+        lspconfig.eslint.setup { capabilities = capabilities }
+        lspconfig.gopls.setup { capabilities = capabilities }
+        lspconfig.lua_ls.setup {
+            capabilities = capabilities,
+            cmd = { "lua-language-server" },
+            settings = {
+                Lua = {
+                    runtime = { version = "Lua 5.1" },
+                    diagnostics = {
+                        globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
                     }
-                end,
+                }
             }
-        })
+        }
+        lspconfig.pyright.setup { capabilities = capabilities } -- Python
+        lspconfig.rnix.setup { capabilities = capabilities } -- Nix
+        lspconfig.rust_analyzer.setup { capabilities = capabilities } -- Rust
+        lspconfig.ts_ls.setup { capabilities = capabilities } -- TypeScript/JavaScript
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require('luasnip').lsp_expand(args.body)
                 end,
             },
             mapping = cmp.mapping.preset.insert({

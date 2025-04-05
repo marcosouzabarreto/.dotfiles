ed5f8f256cf786df22c7a4388a693a3657dbe1f1
Marco Barreto <marcosouzabarreto@gmail.com>
2025-04-05 14:56:24 -0300

    feat: Change cmp to blink

--- nvim/lua/marcobarreto/plugins/lsp.lua
+++ nvim/lua/marcobarreto/plugins/lsp.lua
@@ -1,77 +1,34 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-
-        require("fidget").setup({})
-
-        local lspconfig = require("lspconfig")
-
-        lspconfig.eslint.setup { capabilities = capabilities }
-        lspconfig.gopls.setup { capabilities = capabilities }
-        lspconfig.lua_ls.setup {
-            capabilities = capabilities,
-            cmd = { "lua-language-server" },
-            settings = {
-                Lua = {
-                    runtime = { version = "Lua 5.1" },
-                    diagnostics = {
-                        globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                    }
-                }
+  'neovim/nvim-lspconfig',
+  dependencies = { 'saghen/blink.cmp' },
+  opts = {
+    servers = {
+      eslint = {},
+      tailwindcss = {},
+      gopls = {},
+      lua_ls = {
+        cmd = { "lua-language-server" },
+        settings = {
+          Lua = {
+            runtime = { version = "Lua 5.1" },
+            diagnostics = {
+              globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
             }
+          }
         }
-        lspconfig.pyright.setup { capabilities = capabilities } -- Python
-        lspconfig.rnix.setup { capabilities = capabilities } -- Nix
-        lspconfig.rust_analyzer.setup { capabilities = capabilities } -- Rust
-        lspconfig.ts_ls.setup { capabilities = capabilities } -- TypeScript/JavaScript
+      },
+      pyright = {},
+      nil_ls = {},
+      rust_analyzer = {},
+      ts_ls = {},
+    }
+  },
+  config = function(_, opts)
+    local lspconfig = require('lspconfig')
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-            }, {
-                { name = 'buffer' },
-            })
-        })
-
-        vim.diagnostic.config({
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
+    for server, config in pairs(opts.servers) do
+      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
     end
+  end
 }

32d96af0c0126b60cf1117032e98611ab1c6a300
Marco Barreto <marcosouzabarreto@gmail.com>
2025-06-11 20:30:03 -0300

    feat: Fix formatting with conform.nvim

--- nvim/lua/marcobarreto/plugins/lsp.lua
+++ nvim/lua/marcobarreto/plugins/lsp.lua
@@ -1,34 +1,57 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = { 'saghen/blink.cmp' },
-  opts = {
-    servers = {
-      eslint = {},
-      tailwindcss = {},
-      gopls = {},
-      lua_ls = {
-        cmd = { "lua-language-server" },
-        settings = {
-          Lua = {
-            runtime = { version = "Lua 5.1" },
-            diagnostics = {
-              globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "saghen/blink.cmp",
+    "j-hui/fidget.nvim",
+  },
+
+  config = function()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+
+    require("fidget").setup({})
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "gopls",
+        "pyright",
+      },
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup {
+            capabilities = capabilities
+          }
+        end,
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.lua_ls.setup {
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                runtime = { version = "Lua 5.1" },
+                diagnostics = {
+                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                }
+              }
             }
           }
-        }
-      },
-      pyright = {},
-      nil_ls = {},
-      rust_analyzer = {},
-      ts_ls = {},
-    }
-  },
-  config = function(_, opts)
-    local lspconfig = require('lspconfig')
+        end,
+      }
+    })
 
-    for server, config in pairs(opts.servers) do
-      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-      lspconfig[server].setup(config)
-    end
+    vim.diagnostic.config({
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
   end
 }
